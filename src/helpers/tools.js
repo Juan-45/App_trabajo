d8fbb49009a3ccc59e543b202533a67b1c1528c9
Juan-45 <quijuan12@gmail.com>
2022-08-16 00:11:28 -0300

    Actualizacion

--- src/helpers/tools.js
+++ src/helpers/tools.js
@@ -2,39 +2,117 @@ import Docxtemplater from "docxtemplater";
 import PizZip from "pizzip";
 import { saveAs } from "file-saver";
 
-const generateDocument = (data) => {
-  console.log(data);
-  const getFile = async (callback) => {
-    let fileHandle;
-    // open file picker
-    [fileHandle] = await window.showOpenFilePicker();
-
-    if (fileHandle.kind === "file") {
-      const fileData = await fileHandle.getFile();
-      const arrayBuffer = await fileData.arrayBuffer();
-      // run file code
-      callback(arrayBuffer);
-    } else if (fileHandle.kind === "directory") {
-      // run directory code
-    }
-  };
-
-  getFile(function (content) {
-    const zip = new PizZip(content);
-    const doc = new Docxtemplater(zip, {
-      paragraphLoop: true,
-      linebreaks: true,
-    });
-
-    // render the document (replace all occurences of {first_name} by John, {last_name} by Doe, ...)
-    doc.render(data);
-    const out = doc.getZip().generate({
-      type: "blob",
-      mimeType:
-        "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
-    }); //Output the document using Data-URI
-    saveAs(out, "output.docx");
-  });
+const getFile = async () => {
+  let fileHandle;
+  [fileHandle] = await window.showOpenFilePicker();
+  return fileHandle;
 };
 
-export { generateDocument };
+const getArrayBuffer = async (file) => {
+  const fileData = await file.getFile();
+  const arrayBuffer = await fileData.arrayBuffer();
+  return arrayBuffer;
+};
+
+/*const getFile = (callback, data) => {
+  const file = openFilePicker();
+
+  if (file.kind === "file") {
+    const arrayBuffer = getArrayBuffer(file);
+
+    callback(arrayBuffer, data);
+  } else if (file.kind === "directory") {
+    // run directory code
+  }
+};*/
+
+const getZipInstance = (arrayBuffer) => new PizZip(arrayBuffer);
+
+const getDocxTemplaterInstance = (arrayBuffer, config) =>
+  new Docxtemplater(getZipInstance(arrayBuffer), config);
+
+const getBlob = (docxTemplaterInstance, config) =>
+  docxTemplaterInstance.getZip().generate(config);
+
+const downloadFile = (blob, fileName) => saveAs(blob, fileName);
+
+const testFunction = (arrayBuffer, data) => {
+  const doc = getDocxTemplaterInstance(arrayBuffer, {
+    paragraphLoop: true,
+    linebreaks: true,
+  });
+
+  console.log("instancia", doc);
+
+  doc.render(data);
+  const blob = getBlob(doc, {
+    type: "blob",
+    mimeType:
+      "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+  });
+  downloadFile(blob, "output.docx");
+};
+
+const generateDocument = async (data) => {
+  const file = await getFile();
+  if (file.kind === "file") {
+    const arrayBuffer = await getArrayBuffer(file);
+    testFunction(arrayBuffer, data);
+  }
+};
+
+const getInstanceFromFile = async (callback, config) => {
+  const file = await getFile();
+  if (file.kind === "file") {
+    const arrayBuffer = await getArrayBuffer(file);
+    const doc = getDocxTemplaterInstance(arrayBuffer, config);
+    callback(doc);
+  }
+};
+
+const generateItineratedDocument = (subTemplateInstance) => {
+  subTemplateInstance.render({
+    raw_loop_pagebreak: `<w:br w:type="page"/>`,
+    subTemplateLoop: [
+      {
+        frequency: "2",
+        day: "13",
+        month: "agosto",
+        year: "2022",
+        policeStation: "Seccional Comando Patrulla Pergamino",
+        prosecution: "UFI Y J Nro 4",
+        felony: "Pepe s/ lesiones leves",
+        victim: "María Antonieta",
+        address: "Magallanes 1254",
+        phone: "2477-15457896",
+      },
+      {
+        frequency: "2",
+        day: "13",
+        month: "agosto",
+        year: "2022",
+        policeStation: "Seccional Comando Patrulla Pergamino",
+        prosecution: "UFI Y J Nro 4",
+        felony: "Pepe s/ lesiones leves",
+        victim: "María Antonieta",
+        address: "Magallanes 1254",
+        phone: "2477-15457896",
+        raw_loop_pagebreak: "",
+      },
+    ],
+  });
+  const blob = getBlob(subTemplateInstance, {
+    type: "blob",
+    mimeType:
+      "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+  });
+  downloadFile(blob, "output.docx");
+};
+
+const createRecorridas = () =>
+  getInstanceFromFile(generateItineratedDocument, {
+    paragraphLoop: true,
+    linebreaks: true,
+  });
+
+export { generateDocument, getInstanceFromFile, createRecorridas };
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,4 @@
-import { generateDocument } from "helpers/tools";
+import { generateDocument, createRecorridas } from "helpers/tools";
 import HeaderForm from "./home/HeaderForm";
 import NewVictim from "./home/NewVictim";
 import { Button, Grid, Typography } from "@mui/material";
@@ -7,19 +7,25 @@ import { useState } from "react";
 const Home = () => {
   const [IPPHeader, setIPPHeader] = useState();
   const [victim, setVictim] = useState();
+
+  const [instances, setInstances] = useState({
+    subTemplate: {},
+    mainTemplate: {},
+  });
+
   console.table(IPPHeader);
   console.table(victim);
 
   return (
-    <Grid container justifyContent="center">
+    <Grid container justifyContent='center'>
       <Grid
         container
-        maxWidth="1000px"
-        direction="column"
-        alignItems="center"
+        maxWidth='1000px'
+        direction='column'
+        alignItems='center'
         sx={{ padding: "0px 40px" }}
       >
-        <Typography variant="h4" textAlign="center" gutterBottom>
+        <Typography variant='h4' textAlign='center' gutterBottom>
           Investigación Penal Preparatoria
         </Typography>
         <HeaderForm setIPPHeader={setIPPHeader} />
@@ -27,6 +33,12 @@ const Home = () => {
         <Button onClick={() => generateDocument({ ...IPPHeader, ...victim })}>
           Generate document
         </Button>
+        <Grid container direction='column'>
+          <Typography variant='h4' textAlign='center' gutterBottom>
+            Espacio de pruebas
+          </Typography>
+          <Button onClick={createRecorridas}>Generar recorridas</Button>
+        </Grid>
       </Grid>
     </Grid>
   );
